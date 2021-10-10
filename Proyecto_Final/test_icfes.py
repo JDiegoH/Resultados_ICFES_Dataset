9889943ef685d03fdd96b056c81e1a3a82c02519
JDiegoH <81979990+JDiegoH@users.noreply.github.com>
2021-10-09 20:12:33 -0500

    Add files via upload

--- Proyecto_Final/test_icfes.py
+++ Proyecto_Final/test_icfes.py
@@ -1,5 +1,5 @@
 from Principal import *
 
 def test_abrir_archivo():
-    abrirArchivo('Proyecto_final/Prueba_Saber_11_2019-2.csv')
-    assert verificador()
+    file = Abrir_archivo("Prueba_Saber_11_2019-2.csv")
+    assert verificar(file)
